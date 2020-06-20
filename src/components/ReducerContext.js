d33a88a31b1d97fda7eef363e7ca88ca00907aad
Peter <Posoo617@gmail.com>
2020-06-20 07:54:00 +0300

    adding file for reducer and context hooks

---
+++ src/components/ReducerContext.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function ReducerContext() {
+	return (
+		<div>
+			<h1> Use Reducer with use State </h1>
+			Global state management
+		</div>
+	)
+}
+
+export default ReducerContext
